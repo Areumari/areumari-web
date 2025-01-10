5b752ec43a02cedcbfdf355983ac67e8eab0c7a4
wtei7 <m45a1@dgsw.hs.kr>
2025-01-10 10:57:07 +0900

    feat :: 라우터 주소 추가

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "src/pages/home";
+import Login from "src/pages/auth/login";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     )
