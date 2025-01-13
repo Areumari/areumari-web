9f68202d3ba08b92af2a60cd6ba6b71b93357f21
wtei7 <m45a1@dgsw.hs.kr>
2025-01-13 14:40:14 +0900

    feat :: 테마 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,19 +1,25 @@
 export const lightTheme = {
     colors: {
         main: "#3379D9",
-        secondary: "#ADD8FF",
+        main2: "#ADD8FF",
         error: "#FF4C4C",
         warning: "#f1c40f",
         success: "#2ecc71",
         white: "#FFFFFF",
         light: "#ecf0f1",
-        Gray: '#A0A0A0',
+        gray: '#A0A0A0',
         dark: "#2c3e50",
     },
-    fonts: {
-        large1: {size:"26px",weight:'700'},
-        medium1 : {size:"18px",weight:'600'},
-        mediumSmall1 : {size:"16px",weight:'600'},
-        small1 : {size:"14px",weight:'500'},
+    fontSize: {
+        small1: '0.85vw',
+        mediumSmall1: '1vw',
+        medium1: '1.35vw',
+        large1: '2vw',
+    },
+    fontWeight: {
+        small1: '500',
+        mediumSmall1: '500',
+        medium1: '600',
+        large1: '700',
     }
 };
