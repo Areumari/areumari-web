883f8d9d32095fc5a1be5573450adf0d1aa402ca
wtei7 <m45a1@dgsw.hs.kr>
2025-01-14 19:53:43 +0900

    feat :: 비번 재설정 뷰 추가

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "src/pages/home";
 import Login from "src/pages/auth/login";
 import Signup from "src/pages/auth/signup";
+import ForgotPW from "src/pages/auth/forgotPW";
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/signup' element={<Signup/>}/>
+                <Route path='/forgot-PW' element={<ForgotPW/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/auth/forgotPW/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: end;
+    //background-color: #b1b1b1;
+`
+export const FormContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.light};
+    //box-shadow: 0 4px 4px rgba(0,0,0,0.25);
+`
+export const BackgroundContainer = styled(FormContainer)`
+    
+`
