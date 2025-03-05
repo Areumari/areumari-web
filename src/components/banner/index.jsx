ff5f1d5b827102b15c00fc3369e649787a83b121
wtei7 <m45a1@dgsw.hs.kr>
2025-03-05 11:19:46 +0900

    style :: 배너 디자인 수정

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import * as S from './style';
-import BannerImg from 'src/assets/images/Frame 27.png';
+import BannerImg from 'src/assets/images/Frame 27.jpg';
 
 const Banner = () => {
     return(
-        <S.Wrapper src={BannerImg || null} alt="bener">
-        </S.Wrapper>
+        <S.Wrapper src={BannerImg || null} alt="bener" />
     )
 }
 export default Banner;
